bbc26dded156c44acc6c08d06eddcdb218317cd1
Bui Ngoc Duc <superufom@gmail.com>
2024-08-14 02:38:26 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,6 +3,9 @@
 # Đường dẫn để lưu file boot_ipconfig.sh
 output_file="/home/xpx/vivucloud/boot_ipconfig.sh"
 
+# Đường dẫn để lưu file ipv6.txt
+output_file_ipv6="/home/xpx/vivucloud/ipv6.txt"
+
 # Xóa file cũ nếu đã tồn tại
 rm -f "$output_file"
 
@@ -23,11 +26,13 @@ generate_random_suffix() {
     printf "%04x:%04x:%04x:%04x" $((RANDOM%65536)) $((RANDOM%65536)) $((RANDOM%65536)) $((RANDOM%65536))
 }
 
-# Tạo 500 địa chỉ IPv6 ngẫu nhiên và lưu vào file boot_ipconfig.sh
+# Tạo 500 địa chỉ IPv6 ngẫu nhiên và lưu vào file boot_ipconfig.sh và ipv6.txt
 for i in $(seq 1 500); do
     random_suffix=$(generate_random_suffix)
     echo "ifconfig ens33 inet6 add $prefix$random_suffix/64" >> "$output_file"
+    echo "$prefix$random_suffix" >> "$output_file_ipv6"
 done
+
 
 # Đặt quyền thực thi cho file boot_ipconfig.sh
 chmod +x "$output_file"
