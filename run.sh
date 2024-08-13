2f53ca2dc0d17de0675c5856f9c91db8f077fb1b
Bui Ngoc Duc <superufom@gmail.com>
2024-08-14 03:20:12 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,6 +6,9 @@ output_file="/home/xpx/vivucloud/boot_ipconfig.sh"
 # Đường dẫn để lưu file ipv6.txt
 output_file_ipv6="/home/xpx/vivucloud/ipv6.txt"
 
+# Đường dẫn để lưu file 3proxy.cfg
+output_file_3proxycfg="/etc/3proxy/p3proxy.cfg"
+
 # Xóa file cũ nếu đã tồn tại
 rm -f "$output_file"
 
@@ -26,11 +29,32 @@ generate_random_suffix() {
     printf "%04x:%04x:%04x:%04x" $((RANDOM%65536)) $((RANDOM%65536)) $((RANDOM%65536)) $((RANDOM%65536))
 }
 
+# Ghi các cấu hình cố định vào file 3proxy.cfg
+cat <<EOT >> "$output_file_3proxy"
+daemon
+maxconn 3000
+nserver 1.1.1.1
+nserver [2606:4700:4700::1111]
+nserver [2606:4700:4700::1001]
+nserver [2001:4860:4860::8888]
+nscache 65536
+timeouts 1 5 30 60 180 1800 15 60
+setgid 65535
+setuid 65535
+stacksize 6291456
+flush
+auth none
+EOT
+
 # Tạo 500 địa chỉ IPv6 ngẫu nhiên và lưu vào file boot_ipconfig.sh và ipv6.txt
 for i in $(seq 1 500); do
     random_suffix=$(generate_random_suffix)
     echo "ifconfig ens33 inet6 add $prefix$random_suffix/64" >> "$output_file"
     echo "$prefix$random_suffix" >> "$output_file_ipv6"
+
+    # Tạo dòng cấu hình proxy và lưu vào 3proxy.cfg
+    port=$((14000 + i))
+    echo "proxy -6 -n -a -p$port -i192.168.1.151 -e$prefix$random_suffix" >> "$output_file_3proxy"
 done
 
 
